67328140918ad28903b5823fa3d8dfc7003a1382
grongier <5285-grongier@user.noreply.gitlab.tudelft.nl>
2022-03-23 14:20:31 +0100

    Corrected init file

--- pybarsim/__init__.py
+++ pybarsim/__init__.py
@@ -1,3 +1,3 @@
 """pyBarSim is a Python package to simulate wave-dominated shallow-marine environments using Storms (2003)'s BarSim"""
 
-from pybarsim import BarSim2D, BarSimPseudo3D
+from .pybarsim import BarSim2D, BarSimPseudo3D
